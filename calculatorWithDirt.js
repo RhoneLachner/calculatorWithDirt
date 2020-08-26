afafa36cc3ac5e496d991fbe81fdf15724cbabc3
Rhone Lachner <fernandclay@gmail.com>
2020-08-25 18:58:50 -0700

    second save attempt

---
+++ calculatorWithDirt.js
@@ -0,0 +1,48 @@
+
+// function declaration
+const add = (numOne, numTwo) => {
+    return numOne + numTwo;
+  }
+  // function invocation/call
+  add(1, 2);
+  add(5, 1273547829);
+  
+  
+  const subtract = (numOne, numTwo) => {
+      return numOne - numTwo
+  }
+  //function call subtraction test
+    subtract(5,2);
+  
+  
+  const divide = (numOne, numTwo) => {
+    return numOne / numTwo;
+  }
+  // function call division test
+  divide(2,4);
+  
+  
+  const multiply = (numOne, numTwo) => {
+    return numOne * numTwo
+  }
+  //function call multiplication test
+  multiply(6,6);
+  
+  
+  // !!!Why doesn't this work??!
+  //var array = [add, divide, subtract, multiply] 
+  ////console.log ([0], [1], [2], [3])
+  
+  
+  //console.log ([0], [1], [2], [3])
+  
+  
+  //Only the multiplication aka the last written function shows a result when pressing "run". Why is this?
+  
+  //console.log (add, subtract, divide, multiply) 
+  //console.log (return)
+  
+  
+  
+  changes 
+  
